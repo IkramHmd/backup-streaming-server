fda294e0141c2f3f79dc645e729ee434f557451f
Ikram Hamdi <139885814+IkramHmd@users.noreply.github.com>
2025-08-14 23:18:16 +0100

    copies

--- local-backup.sh
+++ local-backup.sh
@@ -1,4 +1,9 @@
 git clone https://github.com/banago/simple-php-website.git
 cd simple-php-website/
 ls -l 
-sudo apt-get install rsync  
+sudo apt-get install rsync   
+mkdir -p Documents/backup
+# Copies only the contents of folder into dest.
+rsync -zvr simple-php-website/ Documents/backup/
+# Copies the folder itself (with its contents) into dest. 
+sync -zvr simple-php-website  Documents/backup/
